5d21d0dfab83f675bfc13b85991f1c9a9367cbf6
Bisrategebriel Fisseha <42300234+Bisrategebriel@users.noreply.github.com>
2020-11-23 16:23:35 +0300

    Add files via upload

---
+++ Scripts/Menu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    //Start the game
+    public void StartGame() 
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewayForce = 500f;
+
+    // FixedUpdate for smooth physics simulation
+    void FixedUpdate()
+    {
+        // Add forward force
+        // rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        else if (Input.GetKey("a")) {
+            rb.AddForce(-sidewayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        else if (Input.GetKey("w")) {
+            rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        }
+
+        else if (Input.GetKey("d")) {
+            rb.AddForce(0, 0, -(forwardForce) * Time.deltaTime);
+        }
+
+        if(rb.position.y < -1f) {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
